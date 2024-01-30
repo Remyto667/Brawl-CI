ce3c3c757666e25771543c85bf07c855bc2a4d5a
local192 <local192@chipset04.univ-lr.fr>
2024-01-30 14:24:58 +0100

    feat : linter ci

--- public/js/apicall.js
+++ public/js/apicall.js
@@ -1,9 +1,8 @@
 
-const axios = require('axios');
 const url = require('url');
 
 const fixieUrl = url.parse(process.env.FIXIE_URL || 'http://fixie:kmzzVSUwFDuEsju@velodrome.usefixie.com');
-const fixieAuth = fixieUrl.auth.split(':');
+fixieUrl.auth.split(':');
 const apiKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImY5NmQxMmQ5LWI5YjEtNGMzZS1hOWUyLWU0Mjg2M2E1MGMzYSIsImlhdCI6MTY5ODE1OTE2NCwic3ViIjoiZGV2ZWxvcGVyL2NiYmIwMjA0LWNlNWUtY2UwMS1kMDAzLTIzNDgzZjFhNzg3MyIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiODguMTczLjI5LjI0NSIsIjg5LjgxLjE0MC41NyIsIjg5Ljg0LjIwMS4yMiIsIjU0LjE3My4yMjkuMjAwIiwiNTQuMTc1LjIzMC4yNTIiXSwidHlwZSI6ImNsaWVudCJ9XX0.7MDtvwaRT5GNdyQzM8VMR2g2iln68e8OnbGXRAmcnznd-RGXHFd_MK3WzBylGsp05dw11Yrkq2Kx5GewOOFg3Q';
 
 async function fetchDataFromBrawlStarsLocal(playerTag) {
